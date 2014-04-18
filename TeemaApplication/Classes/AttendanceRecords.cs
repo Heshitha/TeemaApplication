43f8b53234ae69ac4fbc16bc96f1f3aced558c6e
Heshitha <heshithachathuranga@gmail.com>
2014-04-18 13:10:20 +0530

    AttendanceRecords

---
+++ TeemaApplication/Classes/AttendanceRecords.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RealandAPI;
+
+namespace TeemaApplication.Classes
+{
+    class AttendanceRecords
+    {
+        public bool GetAllRecordsFromDevice()
+        {
+            Device d = new ZDFinger();
+            try
+            {
+                d.Communication = 2;
+                if (d.OpenCommunication())
+                {
+                    List<Record> allRecords = d.GetAllRecords();
+                    d.CloseCommunication();
+
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return true;
+        }
+    }
+}
