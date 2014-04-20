265b8d969d7c66e59b4b0adfcc8b1e7b73d8f9f2
prabhathanuradha <stpraba@gmail.com>
2014-04-20 11:21:31 +0530

    validate variable Inventive

--- TeemaApplication/VariableIncentiveAllowance.designer.cs
+++ TeemaApplication/VariableIncentiveAllowance.designer.cs
@@ -421,6 +421,7 @@
             this.btnUpdate.TabIndex = 12;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // gbxAfter_No_Pay_Deduction
             // 
